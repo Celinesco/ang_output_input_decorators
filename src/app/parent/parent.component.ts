7d73937f7f49776df2ad1307285fa3517ebd3a67
celines <celina.alsina@gmail.com>
2023-08-25 11:53:11 +0200

    Styled parent component. Added fruit interface. Display parent component

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Fruit } from '../fruits';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
-
+  fruitList: Fruit[] = [
+    {
+      id: 0,
+      name: 'Banane',
+    },
+    { id: 1, name: 'Birne' },
+    { id: 3, name: 'Apfel' },
+  ];
 }
